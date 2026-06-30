fac6ab0217d84b726d3369d6258a685f5e6a925f
Sreehari Annam <sannam@wescodist.com>
2026-06-29 21:17:16 -0400

    Add first 80 PA cities from cities.csv

--- cities.js
+++ cities.js
@@ -1,5 +1,20 @@
 const pennsylvaniaCities = [
-  // ← paste your city list here
+  "Philadelphia", "Harrisburg", "Wallingford", "Altoona", "Orwigsburg",
+  "Scranton", "Exton", "Carbondale", "Alburtis", "Boiling Springs",
+  "Bradford", "West Chester", "Baden", "Bethel Park", "Auburn",
+  "Acme", "Bensalem", "Cabot", "Bentleyville", "Carlisle",
+  "Bainbridge", "Bushkill", "Akron", "Amity", "Boyertown",
+  "Andreas", "Breinigsville", "Bellefonte", "Blanchard", "Butler",
+  "Brownsville", "Buffalo Mills", "Alexandria", "Albion", "Aston",
+  "Annville", "Bryn Mawr", "Campbelltown", "Belle Vernon", "Ambler",
+  "Bangor", "Atglen", "Pittsburgh", "Broomall", "Aliquippa",
+  "Atlasburg", "Berwyn", "Beavertown", "Birchrunville", "Biglerville",
+  "Catasauqua", "Barto", "Callery", "Brookhaven", "Bethlehem",
+  "Aristes", "Burgettstown", "Carrolltown", "Archbald", "Bloomsburg",
+  "Bairdford", "Airville", "Carversville", "Bradfordwoods", "Beach Lake",
+  "Amberson", "Bear Creek", "Bechtelsville", "Blairsville", "California",
+  "Allison Park", "Bally", "Athens", "Blue Bell", "Blakeslee",
+  "Boalsburg", "Cecil", "Birdsboro", "Burlington", "Allentown",
 ];
 
 const BATCH_SIZE = 5;
